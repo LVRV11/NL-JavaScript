dddda214038005106d21d3bb80d56fad849892e7
LVRV11 <lavrovdmitrii@gmail.com>
2022-03-19 00:14:48 +0200

    1js

---
+++ index.js
@@ -0,0 +1,58 @@
+// - Объявите две переменные: admin и user.
+// - Запишите строку "Anna" в переменную user.
+// - Скопируйте значение из переменной user в admin.
+// - Выведите на экран значение admin, используя функцию alert (должна показать Anna).
+
+let admin;
+let user = "Anna";
+admin = user;
+alert (admin);
+
+// Задача 2**: В уроке вы ознакомились с встроенными методами строки которые позволяют 
+// получить срез строки или перевести строку в заглавный регистр. Используя эти 
+// методы преобразуйте строку чтобы первая буква была заглавной.
+// let user = 'ivan';
+// let newUser = ??? // 'Ivan';
+
+let user = "ivan";
+let upString = user.toUpperCase ();
+let firstLetterUser = upString [0];
+let partUser = user.slice (1);
+let newUser = firstLetterUser + partUser;
+console.log (newUser);
+
+// возможно можно так
+
+let user = "ivan";
+let newUser = user[0].toUpperCase() + user.slice (1);
+console.log (newUser);
+
+
+// Задача 3: Тип данных String имеет и другие встроенные методы, используя 
+// поиск в гугл найдите их и приведите примеры их использования
+
+
+// let string = "I love basketball";
+console.log (string.indexOf("love"));
+console.log (string.indexOf("v"));
+console.log (string.indexOf("j"));
+console.log (string.lastIndexOf("l"));
+// // возвращает позицию, на которой располагается совпадение
+console.log (string.includes("l"));
+console.log (string.includes("j"));
+// // возвращает true если есть совпадение, либо false, если нет.
+console.log (string.substr(1,5));
+// Возвращает часть строки
+
+
+// **Задача 4**. Что выведется в следующем примере?:
+let a = '';
+let b = 'bbb';
+let c = false;
+let test1 = !b.length === c; // False (b и c имеют разные типы данных)
+let test2 = a.length > b.length; // False (0 !> 3)
+let test3 = !a.length && !b.length; // True (оператор и)
+let test4 = a === c; // False (a и c имеют разные типы данных)
+
+
+
